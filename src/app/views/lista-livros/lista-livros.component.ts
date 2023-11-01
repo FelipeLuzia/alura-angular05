a25af8dcc5dbf9001e9ad2cc3cb5d51b62ec9e94
Felipe Luzia <felipe.luzia@hotmail.com>
2023-10-31 23:31:19 -0300

    03 05 Renderizando os cards - Pq não está retornando os livros no template?

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -8,18 +9,43 @@ import { LivroService } from 'src/app/service/livro.service';
   styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
   subscription: Subscription;
+  livro: Livro;
 
   constructor(private service: LivroService) {}
 
   buscarLivros(): void {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (retornoAPI) => console.log(),
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoParaLivros(items);
+      },
       error: (erro) => console.error(erro),
-      complete: () => console.log('Observable completado'),
     });
+  }
+
+  // .subscribe(data => this.config = {
+  //   heroesUrl: (data as any).heroesUtl,
+  //   textfile: (data as any).textfile,
+  // });
+
+  livrosResultadoParaLivros(items): Livro[] {
+    const livros: Livro[] = [];
+
+    items.forEach((item) => {
+      livros.push(this.livro = {
+          title: item.VolumeInfo?.title,
+          authors: item.VolumeInfo?.authors,
+          publisher: item.VolumeInfo?.publisher,
+          publishedDate: item.VolumeInfo?.publishedDate,
+          description: item.VolumeInfo?.description,
+          previewLink: item.VolumeInfo?.previewLink,
+          thumbnail: item.VolumeInfo?.imageLinks?.thumbnail,
+        });
+    });
+
+    return livros;
   }
 
   ngOnDestroy(): void {
