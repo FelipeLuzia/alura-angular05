d27fd863d07035ba22ef7d3f562275c9d8c17eef
Felipe Luzia <felipe.luzia@hotmail.com>
2023-11-20 18:44:28 -0300

    06 02 catchError e throwError

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
@@ -16,21 +25,23 @@ export class ListaLivrosComponent {
 
   constructor(private service: LivroService) {}
 
-  livrosEncontrados$ = this.campoBusca.valueChanges
-    .pipe(
-      debounceTime(PAUSA),
-      filter((valorDigitado) => valorDigitado.length >= 3),
-      tap(() => console.log('Fluxo inicial')),
-      distinctUntilChanged(),
-      switchMap(valorDigitado => this.service.buscar
-        (valorDigitado)),
-        tap((retornoAPI) => console.log(retornoAPI)),
-        map(items => this.livrosResultadoParaLivros(items))
-    )
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError((erro) => {
+      console.log(erro);
+      return throwError(() => new Error('Ops, ocorreu um erro'));
+    })
+  );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
-    return items.map(item => {
+    return items.map((item) => {
       return new LivroVolumeInfo(item);
-    })
+    });
   }
 }
